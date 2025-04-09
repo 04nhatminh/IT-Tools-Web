e633482c66adce813f68751cd3a7f3ed42cce425
04nhatminh <04nhatminh@gmail.com>
2025-04-10 06:28:16 +0700

    Add project files.

---
+++ IT-Tools-Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using IT_Tools_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT_Tools_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.HideLayout = true;
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            ViewBag.HideLayout = true;
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TokenGenerator()
+        {
+            return View();
+        }
+        public IActionResult HashText()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
