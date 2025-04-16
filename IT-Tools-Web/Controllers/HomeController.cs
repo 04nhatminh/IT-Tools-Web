4d1e0d38e8ebdcf4f1a9a5d57061f83294dfe15c
phamminh0973 <126540846+phamminh0973@users.noreply.github.com>
2025-04-16 23:55:12 +0700

    VMinh: update HashText

--- IT-Tools-Web/Controllers/HomeController.cs
+++ IT-Tools-Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 using IT_Tools_Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,27 +15,67 @@ namespace IT_Tools_Web.Controllers
             _logger = logger;
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult Login()
         {
             ViewBag.HideLayout = true;
             return View();
         }
+
         public IActionResult Signup()
         {
             ViewBag.HideLayout = true;
             return View();
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
+
         public IActionResult TokenGenerator()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult HashText()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult HashText(string inputText, string algorithm)
+        {
+            try
+            {
+                string hashedResult = ComputeHash(inputText, algorithm);
+                ViewBag.Result = hashedResult;
+            }
+            catch (NotSupportedException ex)
+            {
+                ViewBag.Result = $"❌ Algorithm '{algorithm}' is not supported.";
+            }
+
+            return View();
+        }
+
+        private string ComputeHash(string input, string algorithm)
+        {
+            using HashAlgorithm hasher = algorithm.ToUpper() switch
+            {
+                "MD5" => MD5.Create(),
+                "SHA1" => SHA1.Create(),
+                "SHA256" => SHA256.Create(),
+                "SHA384" => SHA384.Create(),
+                "SHA512" => SHA512.Create(),
+                // Những thuật toán chưa được hỗ trợ mặc định
+                "SHA224" or "SHA3" or "RIPEMD160" => throw new NotSupportedException(),
+                _ => throw new NotSupportedException()
+            };
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = hasher.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
