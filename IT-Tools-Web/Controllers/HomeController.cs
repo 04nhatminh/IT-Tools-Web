f3b0ce8fe12e12de641162b4b64f32137fc34fec
04nhatminh <04nhatminh@gmail.com>
2025-04-24 22:45:38 +0700

    Add Text statistics

--- IT-Tools-Web/Controllers/HomeController.cs
+++ IT-Tools-Web/Controllers/HomeController.cs
@@ -56,6 +56,10 @@ namespace IT_Tools_Web.Controllers
         {
             return View();
         }
+        public IActionResult TextStatistics()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
