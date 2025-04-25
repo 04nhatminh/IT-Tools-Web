336e4c1d49b42e4b1435bc30867ec6b2cb143ae6
phamminh0973 <126540846+phamminh0973@users.noreply.github.com>
2025-04-25 15:35:28 +0700

    VMinh: completed bcrypt

--- IT-Tools-Web/Controllers/HomeController.cs
+++ IT-Tools-Web/Controllers/HomeController.cs
@@ -42,48 +42,16 @@ namespace IT_Tools_Web.Controllers
             return View();
         }
 
-        [HttpGet]
+        public IActionResult Bcrypt()
+        {
+            return View();
+        }
         public IActionResult HashText()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult HashText(string inputText, string algorithm)
-        {
-            try
-            {
-                string hashedResult = ComputeHash(inputText, algorithm);
-                ViewBag.Result = hashedResult;
-            }
-            catch (NotSupportedException ex)
-            {
-                ViewBag.Result = $"❌ Algorithm '{algorithm}' is not supported.";
-            }
 
-            return View();
-        }
-
-        private string ComputeHash(string input, string algorithm)
-        {
-            using HashAlgorithm hasher = algorithm.ToUpper() switch
-            {
-                "MD5" => MD5.Create(),
-                "SHA1" => SHA1.Create(),
-                "SHA256" => SHA256.Create(),
-                "SHA384" => SHA384.Create(),
-                "SHA512" => SHA512.Create(),
-                // Những thuật toán chưa được hỗ trợ mặc định
-                "SHA224" or "SHA3" or "RIPEMD160" => throw new NotSupportedException(),
-                _ => throw new NotSupportedException()
-            };
-
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hashBytes = hasher.ComputeHash(inputBytes);
-            return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
